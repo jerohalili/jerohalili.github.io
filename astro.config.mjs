679e59fdc5318e938e4dd39399664b76ea9a1b38
Jero Halili <127023985+JeroH20@users.noreply.github.com>
2026-04-15 10:03:10 +0800

    update configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://jerohalili.github.io',
-  // base: '/jerohalili.github.io',
   base: '/',
+  // This ensures your projects become /projects/name/index.html
+  build: {
+    format: 'directory'
+  },
+  // Ensure trailing slashes are handled consistently
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
